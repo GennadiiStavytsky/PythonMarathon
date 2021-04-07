861d76a016107ab2f1292558d9896e113c6d39c9
Gennadii <stavytskyigennadii@gmail.com>
2021-04-07 17:12:13 +0300

    Add files via upload

---
+++ 00/t06_casting/cast.py
@@ -0,0 +1,13 @@
+a = 3
+b = 10
+c = -14.8
+d = True
+
+result = float(a + b)
+print("{} + {} = {}".format(a, b, result))
+result = int(c - b)
+print("{} - {} = {}".format(c, b, result))
+result = str(c) + str(b)
+print("{} + {} = {}".format(c, b, result))
+result = bool(a - a)
+print("{} - {} = {}".format(a, a, result))
