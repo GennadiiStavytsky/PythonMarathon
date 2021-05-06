ca3ac96c69613693c2921d7db4d6ae0b872c0834
Gennadii <stavytskyigennadii@gmail.com>
2021-05-06 13:52:55 +0300

    Add files via upload

---
+++ 02/t07_contacts/contacts.py
@@ -0,0 +1,32 @@
+import re
+
+
+def contacts(container, info, operation):
+    reg_name = r'^[a-zA-Z0-9-_ ]*$'
+    reg = r'[^@]*@[^@]*\.[^@]*'
+    opr = ['add', 'update', 'delete']
+    if operation not in opr:
+        return False
+    if 'email' not in info or 'name' not in info:
+        return False
+    if re.match(reg, info['email']) is not None:
+        if re.match(reg_name, info['name']) is not None:
+            key = info.pop('email')
+            if operation == 'add':
+                container.update({key: info})
+                return True
+            elif operation == 'update':
+                if key in container:
+                    container.update({key: {**container.get(key), **info}})
+                    return True
+                else:
+                    return False
+            else:
+                if key in container:
+                    container.pop(key, "none")
+                    return True
+                else:
+                    return False
+    else:
+        return False
+    return True
